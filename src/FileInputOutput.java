810d81832aaf202bf5cdb629faf0c4a7da4aac68
cosmozhao <zhaos22@wfu.edu>
2024-01-31 12:15:21 -0500

    First Try

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -1,6 +1,38 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class FileInputOutput {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream myfile = null;
+        try{
+            myfile = new FileInputStream("src/moviesDataset.csv");
+        }catch(FileNotFoundException e){
+            System.out.println("Could not open file");
+            System.exit(1);
+        }
+        Scanner fileReader = new Scanner(myfile);
+        while(fileReader.hasNextLine()){
+            String data = fileReader.nextLine();
+            System.out.println(data);
+        }
+        fileReader.close();
+        FileInputStream myfile2 = null;
+        try{
+            myfile2 = new FileInputStream("src/moviesDataset.csv");
+        }catch(FileNotFoundException e){
+            System.out.println("Could not open file");
+            System.exit(1);
+        }
+        fileReader = new Scanner(myfile2);
+        while (fileReader.hasNextLine()) {
+            String data = fileReader.nextLine();
+            String[] arr = data.split(",");
+            System.out.println(arr[2]);
+        }
+        fileReader.close();
+    }
+}
 //       1) Open the given file inside src called moviesDataSet.csv
 //       2) Read each row in the dataset
 //       3) Print each row to the screen
@@ -9,6 +41,4 @@ public class FileInputOutput {
 //       6) Close the files after finishing the processing
 //       7) Commit your code and push it to the GitHub
 
-    }
 
-}
