73a276cfe05d0216fb19ff68a44583bc898cafe1
cosmozhao <zhaos22@wfu.edu>
2024-01-31 16:30:20 -0500

    Add year to an output file

--- src/FileInputOutput.java
+++ src/FileInputOutput.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class FileInputOutput {
 
     public static void main(String[] args) throws FileNotFoundException {
-        FileInputStream myfile = null;
+        /*FileInputStream myfile = null;
         try{
             myfile = new FileInputStream("src/moviesDataset.csv");
         }catch(FileNotFoundException e){
@@ -31,8 +31,36 @@ public class FileInputOutput {
             System.out.println(arr[2]);
         }
         fileReader.close();
+         */
+        FileInputStream myfileRead = null;
+        FileOutputStream myFile = new FileOutputStream("YearMade.txt");
+        PrintWriter fileWriter = new PrintWriter(myFile);
+        try {
+            myfileRead = new FileInputStream("src/moviesDataset.csv");
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open file");
+            System.exit(1);
+        }
+        Scanner fileReader = new Scanner(myfileRead);
+        int year;
+        if (fileReader.hasNextLine()) {
+            fileReader.nextLine();
+        }//set the scanner to read from a file
+        while (fileReader.hasNextLine()) {
+            String data = fileReader.nextLine(); //reading lines line by line in data
+            String[] arr = data.split(",");
+            try{
+                    year = Integer.parseInt(arr[4]);
+                    fileWriter.println(year);
+
+            } catch (NumberFormatException e) {
+                fileWriter.println("N/A");
+            }
+        }
+        fileWriter.close();
+        }
+
     }
-}
 //       1) Open the given file inside src called moviesDataSet.csv
 //       2) Read each row in the dataset
 //       3) Print each row to the screen
